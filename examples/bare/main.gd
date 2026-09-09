3e6b5e5616593bcee6e108bdf5fa9f26a559fab9
Ross Hadden <rosshadden@gmail.com>
2026-09-09 16:54:17 -0400

    feat: ptrcall boxes raw args into Variants

--- examples/bare/main.gd
+++ examples/bare/main.gd
@@ -5,3 +5,5 @@ func _ready() -> void:
 	print("gd: is_node2d ", ClassDB.is_parent_class("OiNode2D", "Node2D"))
 	$OiNode.hello()
 	print("gd: add ", $OiNode.add(1, 2))
+	var n: OiNode = $OiNode
+	print("gd: typed add ", n.add(3, 4))
