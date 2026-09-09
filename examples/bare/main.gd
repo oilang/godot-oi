b640dfd904afd203e2efc057aeb1e52033541676
Ross Hadden <rosshadden@gmail.com>
2026-09-09 16:30:07 -0400

    feat: methods take Variant args and return a Variant

--- examples/bare/main.gd
+++ examples/bare/main.gd
@@ -4,3 +4,4 @@ func _ready() -> void:
 	print("gd: can_instantiate ", ClassDB.can_instantiate("OiNode"), " is_node ", ClassDB.is_parent_class("OiNode", "Node"))
 	print("gd: is_node2d ", ClassDB.is_parent_class("OiNode2D", "Node2D"))
 	$OiNode.hello()
+	print("gd: add ", $OiNode.add(1, 2))
